f2309db9c9dca045a936dd4dd612b729fe2ac56a
Jakob Bussas <jakobbu@gmx.de>
2018-05-07 22:21:19 +0200

    no message

--- src/client/app/shared/globals.service.ts
+++ src/client/app/shared/globals.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class GlobalsService {
     // restUrl = 'http://193.22.75.40:50988/forum';
     // restUrl = 'http://localhost:5000/forum';
-    restUrl = 'http://localhost:8080';
+    restUrl = 'http://localhost:8080/api';
     // restUrl = 'https://forumbackend.azurewebsites.net/forum';
 }
