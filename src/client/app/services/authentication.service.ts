4076bc145ef07896bb33ff74873a8cb6ff1e6147
Jakob Bussas <jakobbu@gmx.de>
2018-05-14 14:22:35 +0200

    ka

--- src/client/app/services/authentication.service.ts
+++ src/client/app/services/authentication.service.ts
@@ -10,19 +10,23 @@ export class AuthenticationService {
         private globalsService: GlobalsService
     ) { }
 
-    public login(email: string, password: string): Observable<any> {
-        return this.http.post<any>(this.globalsService.restUrl + '/authenticate',{email: email, password: password}).map(
-            user => {
-                // login successful, if there's a token in the response
-                if (user && user.token) {
-                    // store user details and jwt token in session storage
-                    // to keep user logged in between page refreshes
-                    sessionStorage.setItem('currentUser', JSON.stringify(user));
-                }
-                return user;
-            }
-        );
-    }
+    // public login(email: string, password: string): Observable<any> {
+    //     return this.http.post<any>(this.globalsService.restUrl + '/authenticate',{email: email, password: password}).map(
+    //         user => {
+    //             // login successful, if there's a token in the response
+    //             if (user && user.token) {
+    //                 // store user details and jwt token in session storage
+    //                 // to keep user logged in between page refreshes
+    //                 sessionStorage.setItem('currentUser', JSON.stringify(user));
+    //             }
+    //             return user;
+    //         }
+    //     );
+    // }
+
+    // public isLoggedIn(): boolean {
+        //
+    // }
 
     public logout(): void {
         // remove user from session storage to log user out
