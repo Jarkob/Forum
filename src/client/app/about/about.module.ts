056e1afa226d0c6b3f81762e6ec88abb96148d67
Jakob Bussas <jakobbu@gmx.de>
2018-05-24 21:09:24 +0200

    broken

--- src/client/app/about/about.module.ts
+++ src/client/app/about/about.module.ts
@@ -1,16 +1,18 @@
+import { MatButtonModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { AboutRoutingModule } from './about-routing.module';
-import { MatToolbarModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
     AboutRoutingModule,
-    // MatToolbarModule
+    FormsModule,
+    MatButtonModule
   ],
   declarations: [AboutComponent],
   exports: [AboutComponent]
