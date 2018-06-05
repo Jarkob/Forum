938226278245e2119e34e63fb99f6a1512f71a8f
Jakob Bussas <jakobbu@gmx.de>
2018-06-05 22:23:26 +0200

    fixed date display

--- src/client/app/post-view/post-view.component.ts
+++ src/client/app/post-view/post-view.component.ts
@@ -34,76 +34,6 @@ export class PostViewComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.getPost();
-
-        // without backend mock data
-        // this.post = {
-        //     id: 3,
-        //     title: 'Frage zu Aufgabe 3',
-        //     text: 'Wieso sollen wir Aufgabe 3 machen?',
-        //     status: 'open',
-        //     postTime: '31.03.2009',
-        //     username: 'Diogenes'
-        // };
-
-        // this.comments = [
-        //     {
-        //         id: 1,
-        //         text: 'Verstehe ich auch nicht',
-        //         commentTime: 'eben',
-        //         username: 'Kommentator'
-        //     },
-        //     {
-        //         id: 2,
-        //         text: 'Warum sind wir hier? Woher kommen wir? Wohin gehen wir?' +
-        //         ' Das sind doch wesentlich wichtigere Fragen, die es zu klären gilt!',
-        //         commentTime: 'vor 3 Tagen',
-        //         username: 'Maria Musterfrau'
-        //     },
-        //     {
-        //         id: 3,
-        //         text : 'Wer reitet so spät durch Nacht und Wind?' +
-        //         'Es ist der Vater mit seinem Kind;' +
-        //         'Er hat den Knaben wohl in dem Arm,' +
-        //         'Er fasst ihn sicher, er hält ihn warm.' +
-
-        //         'Mein Sohn, was birgst du so bang dein Gesicht? –' +
-        //         'Siehst, Vater, du den Erlkönig nicht?' +
-        //         'Den Erlenkönig mit Kron’ und Schweif? –' +
-        //         'Mein Sohn, es ist ein Nebelstreif. –' +
-
-        //         '„Du liebes Kind, komm, geh mit mir!' +
-        //         'Gar schöne Spiele spiel’ ich mit dir;' +
-        //         'Manch’ bunte Blumen sind an dem Strand,' +
-        //         'Meine Mutter hat manch gülden Gewand.“ –' +
-
-        //         'Mein Vater, mein Vater, und hörest du nicht,' +
-        //         'Was Erlenkönig mir leise verspricht? –' +
-        //         'Sei ruhig, bleibe ruhig, mein Kind;' +
-        //         'In dürren Blättern säuselt der Wind. –' +
-
-        //         '„Willst, feiner Knabe, du mit mir gehn?' +
-        //         'Meine Töchter sollen dich warten schön;' +
-        //         'Meine Töchter führen den nächtlichen Reihn' +
-        //         'Und wiegen und tanzen und singen dich ein.“ –' +
-
-        //         'Mein Vater, mein Vater, und siehst du nicht dort' +
-        //         'Erlkönigs Töchter am düstern Ort? –' +
-        //         'Mein Sohn, mein Sohn, ich seh’ es genau:' +
-        //         'Es scheinen die alten Weiden so grau. –' +
-
-        //         '„Ich liebe dich, mich reizt deine schöne Gestalt;' +
-        //         'Und bist du nicht willig, so brauch’ ich Gewalt.“ –' +
-        //         'Mein Vater, mein Vater, jetzt faßt er mich an!' +
-        //         'Erlkönig hat mir ein Leids getan! –' +
-
-        //         'Dem Vater grauset’s; er reitet geschwind,' +
-        //         'Er hält in Armen das ächzende Kind,' +
-        //         'Erreicht den Hof mit Mühe und Not;' +
-        //         'In seinen Armen das Kind war tot.',
-        //         commentTime: 'gestern',
-        //         username: 'Johann Wolfgang von Goethe'
-        //     }
-        // ];
     }
 
 
@@ -171,6 +101,7 @@ export class PostViewComponent implements OnInit, OnDestroy {
     }
 
 
+    // TODO: refactor!!!
     private updateComment(id: string): void {
         if (this.editComment) {
             this.editComment = false;
@@ -182,8 +113,6 @@ export class PostViewComponent implements OnInit, OnDestroy {
                     comment = element;
                 }
             });
-
-            // console.log(comment);
 
             this._commentService.updateComment(comment).subscribe();
         } else {
