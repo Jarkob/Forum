5e81ba0777cd65433b24a58f93374b307c930149
Jakob Bussas <jakobbu@gmx.de>
2018-09-06 19:10:17 +0200

    heroku fix

--- src/app/services/globals.service.ts
+++ src/app/services/globals.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalsService {
     // restUrl = 'http://localhost:5000/forum';
-    restUrl = 'http://localhost:8080/api';
-    // restUrl = 'http://forum-backend.herokuapp.com/api';
+    // restUrl = 'http://localhost:8080/api';
+    restUrl = 'http://forum-backend.herokuapp.com/api';
 }
